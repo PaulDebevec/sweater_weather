385849b0b1ae51318f7ef1ad87017411fe6f6c3f
Paul Debevec <paulmdebevec@gmail.com>
2020-06-06 23:02:16 -0600

    Update forecast_request_spec test

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe 'Forecast API' do
 
   it 'sends weather forecast for location', :vcr do
 
-    get '/api/v1/forecast?location=denver,co'
+    get '/api/v1/forecast?location=berthoud,colorado'
 
     expect(response).to be_successful
 
-    result = JSON.parse(response.body, symbolize_names: true)
+    json = JSON.parse(response.body, symbolize_names: true)
     expect(response.status).to eq(200)
+
+    expect(json[:data][:attributes].keys.include?(:today_forecast)).to eq(true)
+    expect(json[:data][:attributes].keys.include?(:weekly_forecast)).to eq(true)
   end
 end

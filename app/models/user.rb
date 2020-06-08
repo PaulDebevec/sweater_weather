8611c4389c5b05fb7b1afdff306d20e523a3109a
Paul Debevec <paulmdebevec@gmail.com>
2020-06-08 15:32:39 -0600

    Add generate_key method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   validates_presence_of :email
   validates_presence_of :password
+
+  def generate_api_key
+    key = KeyGenerator.generate_key
+    self.update(api_key: key)
+  end
 end

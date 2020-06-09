50e4d5b4f81690b551ecae5e6e1e796ee2d9ff3b
Paul Debevec <paulmdebevec@gmail.com>
2020-06-08 18:06:26 -0600

    Change error to hard coded string

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     if user.authenticate(params[:password])
       render json: UsersSerializer.new(user), status: 200
     else
-      render json: {'errors' => user.errors.full_messages.to_sentence}, status: 400
+      render json: {'error' => 'Incorrect email or password'}, status: 400
     end
   end
 end

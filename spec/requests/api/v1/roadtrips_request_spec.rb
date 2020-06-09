0bbdf6246990ee3482c30280ab159c98266262e5
Paul Debevec <paulmdebevec@gmail.com>
2020-06-09 12:25:31 -0600

    Update roadtrips request test

--- spec/requests/api/v1/roadtrips_request_spec.rb
+++ spec/requests/api/v1/roadtrips_request_spec.rb
@@ -8,32 +8,15 @@ RSpec.describe 'Road Trip request' do
     post '/api/v1/road_trip', params: trip_params
 
     expect(response.status).to eq(200)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data][:attributes].keys.include?(:origin)).to eq(true)
+    expect(json[:data][:attributes].keys.include?(:destination)).to eq(true)
+    expect(json[:data][:attributes].keys.include?(:travel_time)).to eq(true)
+    expect(json[:data][:attributes].keys.include?(:forecast)).to eq(true)
+    expect(json[:data][:attributes].keys.include?(:forecast)).to eq(true)
+
+    json_weather = json[:data][:attributes][:forecast][:weather]
+    expect(json_weather.keys.include?(:temp)).to eq(true)
+    expect(json_weather.keys.include?(:description)).to eq(true)
   end
 end
-
-# REQUEST
-# POST /api/v1/road_trip
-# Content-Type: application/json
-# Accept: application/json
-#
-# body:
-#
-# { "origin": "Denver,CO",
-#   "destination": "Pueblo,CO",
-#   "api_key": "jgn983hy48thw9begh98h4539h4"}
-
-# RESPONSE
-# status: 200
-# body:
-# {
-#   "data": {
-#     "type": "road_trip",
-#     "id": 1,
-#     "attributes": {
-#       "origin": 'Denver, CO',
-#       "destination": 'Pueblo, CO',
-#       "travel_time": '1.48 hours',
-#       "arrival_forecast": '74, mostly sunny'
-#     }
-#   }
-# }
